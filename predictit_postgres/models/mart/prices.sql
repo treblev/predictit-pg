77bb9b089ef49be70a143b588fa724d8b315f43d
treblev <vvvcodes@gmail.com>
2025-04-13 11:23:52 -0700

    added contract name instead of id

--- predictit_postgres/models/mart/prices.sql
+++ predictit_postgres/models/mart/prices.sql
@@ -1,12 +1,16 @@
+/* join fact_price_movements with dim_markets */
 with facts as (
     SELECT * FROM {{ ref('fact_price_movements') }}
 ),
 markets as (
     SELECT * FROM {{ ref('dim_markets') }}
+),
+contracts as (
+    SELECT * FROM {{ ref('dim_contracts') }}
 )
 SELECT
     f.market_id,
-    f.contract_id,
+    c.contract_name,
     f.date_id,
     f.last_trade_price,
     f.prev_price,
@@ -16,4 +20,5 @@ SELECT
     m.market_name
 FROM facts f
 JOIN markets m ON f.market_id = m.market_id
+JOIN contracts c ON f.contract_id = c.contract_id
 ORDER BY f.market_id, f.contract_id, f.date_id
